2bdba141de19819514bfb4865b255afd512c83bb
Naseer <feznaseer777@gmail.com>
2026-07-31 22:17:50 +0530

    Added a User distrubution balancer

--- Pybalancer/LB.py
+++ Pybalancer/LB.py
@@ -1,6 +1,9 @@
 from flask import Flask,redirect,url_for
 import requests as REQ
 ##Load balancer 
+from threading import Lock
+lock = Lock
+
 SERVERS=list([
     "http://127.0.0.1:5001",
     "http://127.0.0.1:5002"
@@ -10,6 +13,10 @@ TOTALSERVER=len(SERVERS)
 NEWSERVERS=SERVERS
 NEWNUM=NEWSERVERS
 
+ACTIVE=dict()
+
+for nodes in SERVERS:
+    ACTIVE[nodes]=0
 
 
 
@@ -18,13 +25,35 @@ NEWNUM=NEWSERVERS
 def LOAD():
     ##Here we will send the load balancers
     # return SIMPLEROUNDROBIN()
-    return HEALTHROUNDROBIN()
+    # return HEALTHROUNDROBIN()
+    return USERDIST()
     ...
 
-def HEALTHCHECK():
-    global MOVE,NEWSERVERS,SERVERS,NEWNUM
+
+
+
+def USERDIST():
+    PREV=10*10*10
+    TOSEND=None
+    with lock():
+        global ACTIVE
+        for ip,users in ACTIVE.items():
+            if (users<PREV):
+                    PREV=users
+                    TOSEND=ip
+    if TOSEND is None:
+        return "NULL"
+    return TOSEND
+
+        
+    
+    ...
+    
+    
+
+def __HEALTHCHECK():
+    global MOVE,NEWSERVERS,SERVERS,NEWNUM,ACTIVE
     from time import sleep
-    from threading import Lock
     while True:
         NEWLIST=list()
         ##Check wheter the server is alive or not
@@ -36,17 +65,19 @@ def HEALTHCHECK():
             if RES.status_code==200:
                 NEWLIST.append(nodes) ## I can return here but that will make the the load balcer meaning less
                 lenofserver+=1
+            else:
+                del ACTIVE[nodes]
         ##Now we got the list now time to transer
-        with Lock():
+        with lock():
             
             NEWSERVERS=NEWLIST
             NEWNUM=lenofserver
         sleep(10)
 
 def HEALTHROUNDROBIN():
-    from threading import Lock
+
     global MOVE,NEWSERVERS,NEWNUM
-    with Lock():
+    with lock():
         node= NEWSERVERS[MOVE%NEWNUM]
         MOVE=MOVE+1
     return node
@@ -67,7 +98,7 @@ def SIMPLEROUNDROBIN():
 
 
 from threading import Thread
-Thread(target=HEALTHCHECK,daemon=True).start()
+Thread(target=__HEALTHCHECK,daemon=True).start()
 
 app=Flask(__name__)
 @app.route("/",methods=["GET"])
