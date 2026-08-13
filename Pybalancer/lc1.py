7816358d9577219174cfb391dfdb0ed558910cfb
Naseer <feznaseer777@gmail.com>
2026-08-13 22:45:14 +0530

    Added a health endpoint with random values to stimulate a real servers inconsistency of resources

--- Pybalancer/lc1.py
+++ Pybalancer/lc1.py
@@ -6,9 +6,26 @@ LB1=Flask(__name__)
 
 @LB1.route("/",methods=["GET"])
 def home():
-    return "Hi,From the first server"
+    return {"hello":"Hi,From the first server"}
 
 
+# Because we are testing in this i think it will be better to use random for the gralth
+@LB1.route("/health",methods=["GET"])
+def health():
+    import random
+    from flask import jsonify
+    CPU=random.uniform(70,100)
+    CPUCORES=random.randint(4,8)
+    TOTALRAM=random.randint(8,16)
+    RAMUSAGE=random.uniform(40,80)
+    
+    DATA={
+  "CPU": CPU,
+  "CPUCORES": CPUCORES,
+  "TOTALRAM": TOTALRAM,
+  "RAMUSAGE": RAMUSAGE
+}
+    return jsonify(DATA)
 
 if __name__=="__main__":
     LB1.run(port=5001,debug=True)
--- Pybalancer/lc2.py
+++ Pybalancer/lc2.py
@@ -9,6 +9,24 @@ def home():
     return "Hi,From the second  server"
 
 
+@LB2.route("/health",methods=["GET"])
+def health():
+    import random
+    from flask import jsonify
+    CPU=random.uniform(70,100)
+    CPUCORES=random.randint(4,8)
+    TOTALRAM=random.randint(8,16)
+    RAMUSAGE=random.uniform(40,80)
+    
+    DATA={
+  "CPU": CPU,
+  "CPUCORES": CPUCORES,
+  "TOTALRAM": TOTALRAM,
+  "RAMUSAGE": RAMUSAGE
+}
+    return jsonify(DATA)
+
+
 
 if __name__=="__main__":
     LB2.run(port=5002,debug=True)
